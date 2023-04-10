f600d65a6b5faf6477824b64bd9bce1cecef3225
Witold Michalak <Witold3152000@wp.pl>
2023-04-10 21:12:14 +0200

    list product for nologin users

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Exception;
+
+
+class WelcomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('welcome',[
+            'products'=> Product::paginate(10)
+        ]);
+    }
+}

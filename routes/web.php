7d7ded90865e5267c47806d74da68461f54b251e
Witold Michalak <Witold3152000@wp.pl>
2023-04-22 23:48:08 +0200

    add role user and admin

--- routes/web.php
+++ routes/web.php
@@ -21,26 +21,14 @@ use Illuminate\Support\Facades\Route;
 Auth::routes(['verify' => true]);
 Route::get('/',[WelcomeController::class,'index']);
 Route::middleware(['auth','verified'])->group(function (){
-    Route::resource('products',App\Http\Controllers\ProductController::class);
-    Route::get('/users/list',[App\Http\Controllers\UserController::class,'index'])->middleware('auth');
-    Route::delete('/users/list/{user}',[App\Http\Controllers\UserController::class,'destroy'])->middleware('auth');
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::middleware(['can:isAdmin'])->group(function (){
+         Route::resource('products',App\Http\Controllers\ProductController::class);
+         Route::get('/users/list',[App\Http\Controllers\UserController::class,'index']);
+         Route::delete('/users/list/{user}',[App\Http\Controllers\UserController::class,'destroy']);
+    });
+         Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 });
-// Route::get('/products',[ProductController::class,'index'])->name('products.index')->middleware('auth');
-// 
-// Route::get('/products/create',[ProductController::class,'create'])->name('products.create')->middleware('auth');
-// 
-// Route::get('/products/{product}', [App\Http\Controllers\ProductController::class, 'show'])->name('products.show')->middleware('auth') ;
-// 
-// Route::post('/products',[ProductController::class,'store'])->name('products.store')->middleware('auth');
-// 
-// Route::get('/products/edit/{product}',[ProductController::class,'edit'])->name('products.edit')->middleware('auth');
-// 
-// Route::post('/products/{product}',[ProductController::class,'update'])->name('products.update')->middleware('auth');
-// 
-// Route::delete('/products/{product}',[ProductController::class,'destroy'])->name('products.delete')->middleware('auth');
-
 
 Route::get('/hello',[HelloWorldController::class,'show']);
 
